f63e80e25ecf2aba8f02dc72a2c2f624a41f32a1
Valere21 <62430274+Valere21@users.noreply.github.com>
2023-10-30 23:46:29 +0100

    change lazer behaviour

--- pointeurLazer2.py
+++ pointeurLazer2.py
@@ -2,57 +2,52 @@ import pygame
 import requests
 import json
 import time
+import math
 
 # Configuration Adafruit IO
-ADAFRUIT_IO_KEY = 'aio_iIXw136O8E8SyHjsQEiHupnTX2kf'
+ADAFRUIT_IO_KEY = 'aio_OzEj24OYumLFZAGTLs8QPsEq5QLk'
 ADAFRUIT_IO_USERNAME = 'valval'
 FEED_ID = 'positions'
-URL = f"https://io.adafruit.com/api/v2/{ADAFRUIT_IO_USERNAME}/feeds/{FEED_ID}/data"
+API_URL = f"https://io.adafruit.com/api/v2/{ADAFRUIT_IO_USERNAME}/feeds/{FEED_ID}/data/last"
 
-headers = {
-    "X-AIO-Key": ADAFRUIT_IO_KEY,
+HEADERS = {
+    'X-AIO-Key': ADAFRUIT_IO_KEY,
 }
 
-# Initialisation de pygame
-pygame.init()
-
-# Configuration de la fenêtre
-width, height = 800, 600
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Pointeur Laser")
-
-# Couleur du point (rouge)
-red = (255, 0, 0)
-
 def get_latest_feed_data():
-    response = requests.get(URL, headers=headers)
+    response = requests.get(API_URL, headers=HEADERS)
     if response.status_code == 200:
-        feed_data = json.loads(response.content)
-        if feed_data:
-            value_str = feed_data[0]["value"]
-            print(f"Received data: {value_str}")  # Pour le débogage
-            if value_str:  # Vérifie que la chaîne n'est pas vide
-                try:
-                    latest_data = json.loads(value_str)  # Charger la chaîne JSON en dictionnaire
-                    return latest_data.get('x'), latest_data.get('y')
-                except json.JSONDecodeError:
-                    print(f"Invalid JSON: {value_str}")
-                    return None, None
-            else:
-                print("Received empty JSON string.")
-                return None, None
+        data = json.loads(response.text)
+        value = json.loads(data['value'])
+        return value["x"], value["y"]
     else:
         print(f"Erreur: {response.status_code}")
         return None, None
 
-        
+
+# Initialisation de pygame
+pygame.init()
+
+# Récupérer la résolution de l'écran
+infoObject = pygame.display.Info()
+width, height = infoObject.current_w, infoObject.current_h
+
+# Configuration de la fenêtre
+screen = pygame.display.set_mode((width, height), pygame.NOFRAME)
+pygame.display.set_caption("Pointeur Laser")
+
+
+# Couleur du point (rouge)
+red = (255, 0, 0)
+
 # Initialisation des variables pour le mouvement
 current_x, current_y = width // 2, height // 2
-velocity_x, velocity_y = 0, 0
-jolt_factor = 1  # Facteur d'accélération "jolt"
+target_x, target_y = current_x, current_y
+speed = 0.05  # Vitesse de déplacement du pointeur vers la cible
 
 # Boucle principale
 running = True
+start_time = time.time()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -60,38 +55,34 @@ while running:
 
     # Récupérer les données depuis Adafruit IO
     delta_x, delta_y = get_latest_feed_data()
-    
+
+    elapsed_time = (time.time() - start_time) % 1  # Normalisé entre 0 et 1
+    sigmoid = 1 / (1 + math.exp(-10*(elapsed_time - 0.5)))
+
     if delta_x is not None and delta_y is not None:
+        target_x, target_y = delta_x, delta_y
+
+        # Calcul de la différence entre la position actuelle et la cible
+        diff_x = target_x - current_x
+        diff_y = target_y - current_y
+
+        # Mise à jour des positions en fonction de la vitesse
+        current_x += diff_x * speed
+        current_y += diff_y * speed
+        # Écrire la position du point
+        print(f"Position du curseur : x = {current_x}, y = {current_y}")
+
+        # Effacer l'écran
+        screen.fill((0, 0, 0, 0))  # Remplir avec une couleur transparente
         
-        # Ignorer les valeurs en dehors de l'intervalle [-128, 127]
-        if -128 <= delta_x <= 127 and -128 <= delta_y <= 127:
-            
-            # Appliquer la fonction de jolt
-            velocity_x += jolt_factor * delta_x
-            velocity_y += jolt_factor * delta_y
-            
-            # Mettre à jour les positions
-            current_x += velocity_x
-            current_y += velocity_y
+        # Dessiner le point rouge à la position calculée
+        pygame.draw.circle(screen, red, (current_x, current_y), 5)
 
-            # Restreindre les coordonnées à l'intérieur de la fenêtre
-            current_x = max(0, min(width, current_x))
-            current_y = max(0, min(height, current_y))
+        # Mettre à jour l'affichage
+        pygame.display.flip()
 
-            # Écrire la position du point
-            print(f"Position du curseur : x = {current_x}, y = {current_y}")
-
-            # Effacer l'écran
-            screen.fill((0, 0, 0))
-
-            # Dessiner le point rouge à la position calculée
-            pygame.draw.circle(screen, red, (current_x, current_y), 5)
-
-            # Mettre à jour l'affichage
-            pygame.display.flip()
-
-    # Attendre 1 seconde avant la prochaine requête
-    time.sleep(1)
+    # Attendre avant la prochaine requête
+    time.sleep(0.05)  # Réduisez cette valeur pour une animation plus fluide
 
 # Quitter pygame
 pygame.quit()
